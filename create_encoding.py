4ab0d4e74973f2cc7c0082f494169665a32e2d18
Vysakh <33203281+vyzboy92@users.noreply.github.com>
2019-03-26 13:01:11 +0530

    Add files via upload

---
+++ create_encoding.py
@@ -0,0 +1,25 @@
+import face_recognition
+import cv2
+import argparse
+import pymongo
+
+em_client = pymongo.MongoClient("mongodb://localhost:27017/")
+dblist = em_client.list_database_names()
+if "Main_DB" in dblist:
+    print("========================")
+    print("Main_db found in Mongo")
+    print("========================")
+em_db = em_client["Main_DB"]
+em_col = em_db["face_info"]  # logs face encodings as arrays with corresponding name
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input_image", required=True,
+                help="path to input image")
+ap.add_argument("-n", "--name", required=True,
+                help="name of user", type=str)
+args = vars(ap.parse_args())
+image = face_recognition.load_image_file(args["input_image"])
+face_encoding = face_recognition.face_encodings(image)[0]
+data = {'name': args["name"], 'encoding': [list(face_encoding)]}
+em_col.insert(data)
+
